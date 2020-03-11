f34df84df6f8600692b0ee9f3ecf8f33bce6e471
jasonlbx13 <Boxli940808>
2020-03-11 16:48:51 +0800

    first commit

---
+++ Week_01/G20200343040026/LeetCode_189_0026.py
@@ -0,0 +1,48 @@
+# 解法一:三次翻转
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) <= 1:
+            return
+        
+        k = k%len(nums) if k>=len(nums) else k
+        if k==0:
+            return
+
+        nums[0:-k] = nums[0:-k][::-1]
+        nums[-k:] = nums[-k:][::-1]
+        nums[:] = nums[::-1]
+        return
+
+# 解法二:循环填放
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) <= 1:
+            return
+        
+        k = k%len(nums) if k>=len(nums) else k
+        if k==0:
+            return
+        i = 0
+        index = k
+        insert = nums[0]
+        flag = 0
+
+        for i in range(len(nums)):
+            tmp = nums[index]
+            nums[index] = insert
+            if index==flag:
+                index += 1
+                tmp = nums[index]
+                flag += 1
+            insert = tmp
+            index = (index+k)%len(nums)
+
+
+        return
