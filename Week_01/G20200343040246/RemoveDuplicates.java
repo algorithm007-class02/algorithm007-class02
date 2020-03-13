1a2c96643d3031733e3d4e7d65547335a8468b13
liuzhenbin <liuzhenbin@megvii.com>
2020-03-13 18:32:35 +0800

    week1 homework

--- Week_01/G20200343040246/RemoveDuplicates.java
+++ Week_01/G20200343040246/RemoveDuplicates.java
@@ -1,15 +1,25 @@
-class Solution {
-    public int removeDuplicates(int[] nums) {
-        // 1. double point, index change O(n)
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        int i = 0;
-        for (int j = 1; j < nums.length; j++) {
-            if (nums[j] != nums[i]) {
-                nums[++i] = nums[j];
+package Week_01.G20200343040246;
+
+public class RemoveDuplicates {
+    public static void main(final String[] args) {
+        Solution solution = new RemoveDuplicates().new Solution();
+        int[] nums = new int[]{0,0,1,1,1,2,2,3,3,4};
+        solution.removeDuplicates(nums);
+    }
+
+    class Solution {
+        public int removeDuplicates(int[] nums) {
+            // 1. double point, index change O(n)
+            if (nums == null || nums.length == 0) {
+                return 0;
             }
+            int i = 0;
+            for (int j = 1; j < nums.length; j++) {
+                if (nums[j] != nums[i]) {
+                    nums[++i] = nums[j];
+                }
+            }
+            return i + 1;
         }
-        return i + 1;
     }
 }
