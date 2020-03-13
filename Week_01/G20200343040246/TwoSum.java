661c06c5a1157086dea4b56de01af0fc922c6668
liuzhenbin <liuzhenbin@megvii.com>
2020-03-13 18:20:47 +0800

    week1 homework

---
+++ Week_01/G20200343040246/TwoSum.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // 1: two-pass loop  O(n^2)
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        // 2: two-pass hash O(n)
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff) && map.get(diff) != i) {
+                return new int[]{i, map.get(diff)};
+            }
+        }
+        return new int[]{};
+    }
+
+    public int[] twoSum3(int[] nums, int target) {
+        // 3: one-pass hash O(n)
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff)) {
+                return new int[]{map.get(diff), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+}
