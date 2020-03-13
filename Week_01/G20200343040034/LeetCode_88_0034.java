411fefaff53208cb5d7f5d3a70173e68da580575
zhaominglang <zhaominglang@mafengwo.com>
2020-03-13 18:11:01 +0800

    0034 Week1

---
+++ Week_01/G20200343040034/LeetCode_88_0034.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if(nums1.length == 0 || nums2.length == 0) return;
+        int indexM = m-1,indexN = n-1,length = m+n,last = length-1;
+        while(last >= 0){
+            if(indexM < 0) {
+                nums1[last--] = nums2[indexN--];
+                continue;
+            }
+            if(indexN < 0) break;
+            if(nums1[indexM] > nums2[indexN]){
+                nums1[last--] = nums1[indexM--];
+            }else{
+                nums1[last--] = nums2[indexN--];
+            }
+        }
+    }
+}
