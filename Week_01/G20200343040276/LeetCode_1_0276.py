c952729682980ef476f284e7c4a5d2e456c3a849
马佳栋 <ma857714903@gmail.com>
2020-03-15 21:59:04 +0800

    week01_homework

---
+++ Week_01/G20200343040276/LeetCode_1_0276.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode.cn id=1 lang=python3
+#
+# [1] 两数之和
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
+# @lc code=end
+
---
+++ Week_01/G20200343040276/LeetCode_88_0276.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=88 lang=python3
+#
+# [88] 合并两个有序数组
+#
+
+# @lc code=start
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        while m>0 and n>0:
+            if nums1[m-1] >= nums2[n-1]:
+                nums1[m+n-1]=nums1[m-1]
+                m-=1
+            else:
+                nums1[m+n-1]=nums2[n-1]
+                n-=1
+        if n>0:
+            nums1[:n]=nums2[:n]
+# @lc code=end
+
