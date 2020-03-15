fb0e973fadab786a46c1efe10f22a2a4ff92051b
wangwb20448 <wangwb20448@hundsun.com>
2020-03-15 13:35:05 +0800

    0270_Week01(java) 修改文件名称的题目序号

--- Week_01/G20200343040270/LeetCode_1_0270.java
+++ Week_01/G20200343040270/LeetCode_1_0270.java
@@ -1,25 +1,50 @@
 /**
- * 删除排序数组中的重复项：
- * 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
- * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ * 两数之和：
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
  */
 class Solution {
+
     /**
-     *
-     * 时间复杂度：O(n)
+     * 解法一:暴力解法，两两相加，满足则返回结果
+     * 时间复杂度：O(n^2)
      * 空间复杂度：O(1)
      *
-     * @param nums 排序数组
-     * @return 移除后数组的新长度
+     * @param nums   整数数组
+     * @param target 目标值
+     * @return 角标集合
      */
-    public int removeDuplicates(int[] nums) {
-        int i = 0;
-        for (int j = 1; j < nums.length; j++) {
-            //如果i和j相等则不需要交换
-            if (nums[i] != nums[j] && ++i != j) {
-                nums[i] = nums[j];
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    res[0] = i;
+                    res[1] = j;
+                    return res;
+                }
             }
         }
-        return i + 1;//i是角标，长度需+1
+        return res;
+    }
+
+    /**
+     * 解法二：
+     *
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(n)
+     */
+    public int[] twoSum2(int[] nums, int target) {
+        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(!map.containsKey(target - nums[i])){
+                map.put(nums[i],i);
+            }else {
+                res[0] = map.get(target - nums[i]);
+                res[1] = i;
+                return res;
+            }
+        }
+        return res;
     }
 }
