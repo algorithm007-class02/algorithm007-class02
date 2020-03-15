685d16518e9c0e9b835a0245bfa7c4cdcb9149d9
james <13188961863@163.com>
2020-03-15 11:23:00 +0800

    第一周作业修改

--- Week_01/G20200343040040/LeetCode_26_0040.java
+++ Week_01/G20200343040040/LeetCode_26_0040.java
@@ -2,6 +2,7 @@ class Solution {
     public int removeDuplicates(int[] nums) {
         if (nums.length == 0) return 0;
         int index = 0;
+
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] != nums[index]){
                 index++;
