aad548d950439d952acf6ff34f0495bf25f29ea0
lijiakui <lijk@smartbow.net>
2020-03-15 23:16:19 +0800

    完成最后两题

--- Week_01/G20200343040214/LeetCode_25_214.java
+++ Week_01/G20200343040214/LeetCode_25_214.java
@@ -1,2 +1,42 @@
 public class Solution25 {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode cache = new ListNode(0);
+        cache.next = head;
+        ListNode pre = cache;
+        ListNode end = cache;
+        while (end.next != null) {
+            // 分段处理翻转，翻转完成后拼接在进行下一组
+            for (int i = 0; i < k && end != null; i++) {
+                end = end.next;
+            }
+            if (end == null) {
+                break;
+            }
+            ListNode splitStart = pre.next;
+            ListNode next = end.next;
+            end.next = null; // 截断
+            pre.next = reverseList(splitStart);
+            // 拼接
+            splitStart.next = next;
+            pre = splitStart;
+            end = pre;
+        }
+        return cache.next;
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        while (curr != null) {
+            // 暂存 next
+            ListNode temp = curr.next;
+            // 断开
+            curr.next = prev;
+            // 重组
+            prev = curr;
+            // 后移动
+            curr = temp;
+        }
+        return prev;
+    }
 }
