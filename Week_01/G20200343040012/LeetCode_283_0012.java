37f70aa447d40e5006c126731c0ab613f59561d5
王戎 <88343264@qq.com>
2020-03-15 20:57:13 +0800

    Update 1、week_01 283 调整，增加多种解法。

--- Week_01/G20200343040012/LeetCode_283_0012.java
+++ Week_01/G20200343040012/LeetCode_283_0012.java
@@ -1,4 +1,9 @@
 class Solution {
+
+    /**
+     * 直接赋值为0
+     * @param nums
+     */
     public void moveZeroes(int[] nums) {
         if (null == nums || nums.length == 0) {
             return;
@@ -7,11 +12,33 @@ class Solution {
         int moveIndex = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                nums[moveIndex] = nums[i];
                 if (i != moveIndex) {
+                    nums[moveIndex] = nums[i];
                     nums[i] = 0;
                 }
                 moveIndex++;
+            }
+        }
+    }
+
+    /**
+     * 交换方式
+     * @param nums
+     */
+    public void moveZeroesSecond(int[] nums) {
+        if (null == nums || nums.length == 0) {
+            return;
+        }
+
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                if (i != j) {
+                    int temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
+                }
+                j++;
             }
         }
     }
