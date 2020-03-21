92ac7e566289552567084e09bb78cfb5326d5727
Zhang Rui <windknife@vip.qq.com>
2020-03-21 20:04:03 +0800

    提交第二周作业

--- Week_02/G20190343010122/LeetCode_49_0122.go
+++ Week_02/G20190343010122/LeetCode_49_0122.go
@@ -2,20 +2,6 @@ package homework
 
 import "sort"
 
-type stringSorter []rune
-
-func (s stringSorter)Len()int{
-	return len(s)
-}
-
-func (s stringSorter)Less(i,j int) bool{
-	return s[i] < s[j]
-}
-
-func (s stringSorter)Swap(i,j int){
-	s[i],s[j] = s[j],s[i]
-}
-
 func groupAnagrams(strs []string)[][]string{
 	result := make([][]string, 0)
 	anagramMap := make(map[string][]string, 0)
@@ -38,4 +24,18 @@ func groupAnagrams(strs []string)[][]string{
 	}
 
 	return result
+}
+
+type stringSorter []rune
+
+func (s stringSorter)Len()int{
+	return len(s)
+}
+
+func (s stringSorter)Less(i,j int) bool{
+	return s[i] < s[j]
+}
+
+func (s stringSorter)Swap(i,j int){
+	s[i],s[j] = s[j],s[i]
 }
