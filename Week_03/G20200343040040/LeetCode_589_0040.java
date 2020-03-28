b4bcf6ac7ba00df1b868ae791bcba05e81e6beeb
zhangzhao <zhangzhao0369@hhdd.com>
2020-03-28 11:08:28 +0800

    第三周作业提交

---
+++ Week_03/G20200343040040/LeetCode_589_0040.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Integer> preorder(Node root) {
+        LinkedList<Node> stack = new LinkedList<>();
+        LinkedList<Integer> output = new LinkedList<>();
+        if (root == null) {
+            return output;
+        }
+
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            Node node = stack.pollLast();
+            output.add(node.val);
+            Collections.reverse(node.children);
+            for (Node item : node.children) {
+                stack.add(item);
+            }
+        }
+        return output;
+    }
+}
