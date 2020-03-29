1727be5a162916fcb02c048ee7030634d1c57d0e
wangqx <wangqx@litsoft.com>
2020-03-29 20:07:14 +0800

    qq

---
+++ Week_03/G20200343040174/LeetCode_589_0174.java
@@ -0,0 +1,25 @@
+// 前序顺序遍历
+// https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal/description/
+
+public List<Integer> preorder(Node root) {
+
+        if(root == null){
+        return list;
+        }
+
+        list.add(root.val);
+
+        if(root.children == null){
+
+        return list;
+        }
+
+
+        for (int i = 0; i < root.children.size(); i++) {
+        preorder(root.children.get(i));
+        }
+
+
+
+        return list;
+        }
