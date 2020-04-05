9d7f4145b8449128f5fe1433ac830b7dadd2818e
hongyangliao <hongyangliao163@163.com>
2020-04-05 17:19:04 +0800

    Update LeetCode_153_0250.java

--- Week_04/G20200343040250/LeetCode_153_0250.java
+++ Week_04/G20200343040250/LeetCode_153_0250.java
@@ -1,12 +1,12 @@
 class Solution {
     public int findMin(int[] nums) {
         int len = nums.length;
-        if (len == 0) throw new RuntimeException(nums is empty);
+        if (len == 0) throw new RuntimeException("nums is empty");
         int start = 0, end = len - 1;
 
         while (start < end) {
-            int mid = (start + end)  1;
-            if (nums[mid] <=  nums[end]) {
+            int mid = (start + end) >>> 1;
+            if (nums[mid] > nums[end]) {
                 start = mid + 1;
             } else {
                 end = mid;
