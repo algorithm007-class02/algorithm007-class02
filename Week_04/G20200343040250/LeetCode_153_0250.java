c785f1913148076bf3bdddac31409c9f299db0ef
hongyangliao <hongyangliao163@163.com>
2020-04-05 17:15:38 +0800

    Update LeetCode_153_0250.java

--- Week_04/G20200343040250/LeetCode_153_0250.java
+++ Week_04/G20200343040250/LeetCode_153_0250.java
@@ -4,9 +4,9 @@ class Solution {
         if (len == 0) throw new RuntimeException(nums is empty);
         int start = 0, end = len - 1;
 
-        while (start  end) {
+        while (start < end) {
             int mid = (start + end)  1;
-            if (nums[mid]  nums[end]) {
+            if (nums[mid] <=  nums[end]) {
                 start = mid + 1;
             } else {
                 end = mid;
