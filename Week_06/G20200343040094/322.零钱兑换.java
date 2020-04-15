dc0331c742f19bc2f5fe8ca6c383d24de8b00258
VarianM <mizeyuyu@gmail.com>
2020-04-16 00:34:00 +0800

    update

---
+++ Week_06/G20200343040094/322.零钱兑换.java
@@ -0,0 +1,66 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode.cn id=322 lang=java
+ *
+ * [322] 零钱兑换
+ *
+ * https://leetcode-cn.com/problems/coin-change/description/
+ *
+ * algorithms
+ * Medium (38.79%)
+ * Likes:    542
+ * Dislikes: 0
+ * Total Accepted:    74.3K
+ * Total Submissions: 191.1K
+ * Testcase Example:  '[1,2,5]\n11'
+ *
+ * 给定不同面额的硬币 coins 和一个总金额
+ * amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: coins = [1, 2, 5], amount = 11
+ * 输出: 3 
+ * 解释: 11 = 5 + 5 + 1
+ * 
+ * 示例 2:
+ * 
+ * 输入: coins = [2], amount = 3
+ * 输出: -1
+ * 
+ * 
+ * 
+ * 说明:
+ * 你可以认为每种硬币的数量是无限的。
+ * 
+ */
+
+// @lc code=start
+
+// 1.递归
+// 2.DP
+//     a.分治 minStep(amount) = MIN(minStep(amount), minStep(amount-coin)+1)
+//     b.状态定义 f[i] 凑成金额i所需要的最少硬币数
+//     c.DP方程 dp[i] = min(dp[i], dp[i-coin]+1)
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if (coins.length == 0)
+            return -1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; ++i) {
+            for (int coin : coins) {
+                if (i >= coin) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
+// @lc code=end
