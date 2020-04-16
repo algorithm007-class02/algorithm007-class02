87c19fec9e5cc0b5e3eb759d4773a83ed6e9b561
VarianM <mizeyuyu@gmail.com>
2020-04-16 21:53:56 +0800

    update

---
+++ Week_06/G20200343040094/213.打家劫舍-ii.java
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=213 lang=java
+ *
+ * [213] 打家劫舍 II
+ *
+ * https://leetcode-cn.com/problems/house-robber-ii/description/
+ *
+ * algorithms
+ * Medium (37.09%)
+ * Likes:    238
+ * Dislikes: 0
+ * Total Accepted:    29.4K
+ * Total Submissions: 78.7K
+ * Testcase Example:  '[2,3,2]'
+ *
+ * 
+ * 你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都围成一圈，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+ * 
+ * 给定一个代表每个房屋存放金额的非负整数数组，计算你在不触动警报装置的情况下，能够偷窃到的最高金额。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [2,3,2]
+ * 输出: 3
+ * 解释: 你不能先偷窃 1 号房屋（金额 = 2），然后偷窃 3 号房屋（金额 = 2）, 因为他们是相邻的。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [1,2,3,1]
+ * 输出: 4
+ * 解释: 你可以先偷窃 1 号房屋（金额 = 1），然后偷窃 3 号房屋（金额 = 3）。
+ * 偷窃到的最高金额 = 1 + 3 = 4 。
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+        int most = Math.max(maxRob(nums, 0, n-2), maxRob(nums, 1, n-1));
+        return most;
+    }
+    private int maxRob(int[] nums, int start, int end) {
+        int prevMax = 0, currMax = 0;
+        for (int i = start; i <= end; ++i) {
+            int tmp = currMax;
+            currMax = Math.max(prevMax + nums[i], currMax);
+            prevMax = tmp;
+        }
+        return currMax;
+    }
+}
+// @lc code=end
+
