dd69b47cdf59457af73d1f0a11e2869c02d11f80
varian <mizeyuyu@gmail.com>
2020-04-18 15:11:33 +0800

    update

--- Week_06/G20200343040094/213.打家劫舍-ii.java
+++ Week_06/G20200343040094/213.打家劫舍-ii.java
@@ -38,11 +38,11 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if(n == 0) return 0;
-        if(n == 1) return nums[0];
-        int most = Math.max(maxRob(nums, 0, n-2), maxRob(nums, 1, n-1));
+        if (n == 1) return nums[0];
+        int most = Math.max(maxRob(nums, 0, n - 2), maxRob(nums, 1, n - 1));
         return most;
     }
+
     private int maxRob(int[] nums, int start, int end) {
         int prevMax = 0, currMax = 0;
         for (int i = start; i <= end; ++i) {
@@ -54,4 +54,3 @@ class Solution {
     }
 }
 // @lc code=end
-
