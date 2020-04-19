d815d7d806e22dc02fc4682ebac03078dfa68e74
Tears <i@ltteam.cn>
2020-04-19 22:40:37 +0800

    week06 homework

--- Week_06/G20200343040028/LeetCode_32_0028.go
+++ Week_06/G20200343040028/LeetCode_32_0028.go
@@ -1,49 +1,47 @@
 package G20200343040028
+/**
+1. 最长 关键字 可以联想到 使用 贪心 或者 动态规划
+2. 思考此类问题如果不能一眼看出答案,可以先思考解空间搜索法:
+    1)逐一遍历所有子串
+    2)判断是否为有效括号
+    3)存储最长值即可
+3.  寻找重叠子问题发现,重复判断同一段子串是否有效,因此我们可以使用 dp数组缓存一段有效子串的长度
+    那么分为几种情况:
+        1) 当前字符为')'时,一定构不成有效子串直接忽略
+        2) 当前字符为'('时
+            a.s[i-1] 为'('
+                则 i==0时 dp[i] = 2
+                   i>0时  dp[i] = dp[i-1] + 2
+            b. s[i-1] 为'('
+                则  i - dp[i - 1] - 1 == 0 时
+                      dp[i] = dp[i-1] + 2
+                    i - dp[i - 1] - 1 != 0 时
+                      dp[i] = dp[i-1] + 2 + dp[i-dp[i-1]-2]
+4. 此题关键在于通过重叠问题确定dp数组的含义,并定义循环不变式,再次基础上 正确推导出 dp方程,然后转换为编码即可
+*/
 
-func minPathSum(grid [][]int) int {
-	// 取纵向
-	m := len(grid)
-	// 取横向
-	n := len(grid[0])
-
-	if m == 0 {
-		return 0
-	}
-
-	// 定义一个二维切片来存起点到该点的最小值
-	dp := make([][]int,m)
-
-	for i:=0;i<m;i++ {
-		dp[i] = make([]int,n)
-		for j:=0;j<n;j++ {
-			var left, top int
-			if i == 0 && j == 0 {
-				dp[0][0] = grid[0][0]
-				continue
+func longestValidParentheses(s string) int {
+	dp, res := make([]int,len(s)), 0
+	// dp 数组表示 当前位置i处 之前在s串中构造的有效括号长度
+	for i,_ := range s{
+		if i > 0 &&  s[i] == ')'{
+			if s[i - 1] == '('{
+				if i == 1{
+					dp[i] = 2
+				}else{
+					dp [i] = dp[i - 2] + 2
+				}
+			}else if s[i - 1] == ')' &&  i - dp[i - 1] - 1 >= 0   &&  s[i-dp[i-1]-1] == '('  {
+				if  i - dp[i - 1] - 1 == 0{
+					dp[i] = dp[i-1] + 2
+				}else{
+					dp[i] = dp[i-1] + 2 + dp[i-dp[i-1]-2]
+				}
 			}
-			if i == 0 {
-				dp[0][j] = dp[0][j-1]+grid[i][j]
-				continue
-			}  else {
-				left = dp[i-1][j]
+			if dp[i] > res{
+				res = dp[i]
 			}
-
-			if j == 0 {
-				dp[i][0] = dp[i-1][0]+grid[i][j]
-				continue
-			} else {
-				top = dp[i][j-1]
-			}
-			dp[i][j] = min(left,top)+grid[i][j]
 		}
 	}
-	return dp[m-1][n-1]
+	return res
 }
-
-func min(a,b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
--- Week_06/G20200343040028/LeetCode_64_0028.go
+++ Week_06/G20200343040028/LeetCode_64_0028.go
@@ -1,33 +1,49 @@
 package G20200343040028
 
-import "sort"
+func minPathSum(grid [][]int) int {
+	// 取纵向
+	m := len(grid)
+	// 取横向
+	n := len(grid[0])
 
-func groupAnagrams(strs []string) [][]string {
-	if strs == nil || len(strs) == 0 {
-		return [][]string{}
+	if m == 0 {
+		return 0
 	}
-	rMap := make(map[string][]int, 0)
-	for i, v := range strs {
-		str := aSort(v)
-		if _, ok := rMap[str]; !ok {
-			rMap[str] = []int{i}
-		} else {
-			rMap[str] = append(rMap[str], i)
+
+	// 定义一个二维切片来存起点到该点的最小值
+	dp := make([][]int,m)
+
+	for i:=0;i<m;i++ {
+		dp[i] = make([]int,n)
+		for j:=0;j<n;j++ {
+			var left, top int
+			if i == 0 && j == 0 {
+				dp[0][0] = grid[0][0]
+				continue
+			}
+			if i == 0 {
+				dp[0][j] = dp[0][j-1]+grid[i][j]
+				continue
+			}  else {
+				left = dp[i-1][j]
+			}
+
+			if j == 0 {
+				dp[i][0] = dp[i-1][0]+grid[i][j]
+				continue
+			} else {
+				top = dp[i][j-1]
+			}
+			dp[i][j] = min(left,top)+grid[i][j]
 		}
 	}
-	res := make([][]string, 0)
-	for _, v := range rMap {
-		a := make([]string, 0)
-		for _, x := range v {
-			a = append(a, strs[x])
-		}
-		res = append(res, a)
-	}
-	return res
+	return dp[m-1][n-1]
 }
 
-func aSort(s string) string{
-	x := []byte(s)
-	sort.Slice(x, func(i, j int) bool {return x[i] < x[j]})
-	return string(x)
+func min(a,b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
+
