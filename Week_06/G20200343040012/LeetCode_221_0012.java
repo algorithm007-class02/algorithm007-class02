a4737670169229726099d5bcb98dd350374d60cf
王戎 <88343264@qq.com>
2020-04-19 22:35:24 +0800

    update 1.第六周习题。

---
+++ Week_06/G20200343040012/LeetCode_221_0012.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public int maximalSquare(char[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] dp = new int[rows + 1][cols + 1];
+        int maxLen = 0;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j - 1]), dp[i - 1][j]) + 1;
+                    maxLen = Math.max(maxLen, dp[i][j]);
+                }
+            }
+        }
+        return maxLen * maxLen;
+    }
+
+}
---
+++ Week_06/G20200343040012/LeetCode_647_0012.java
@@ -0,0 +1,50 @@
+class Solution {
+
+    public int countSubstrings(String s) {
+        int n = s.length();
+        int ans = 0;
+        for (int center = 0; center <= 2 * n - 1; center++) {
+            int left = center / 2;
+            int right = left + center % 2;
+
+            while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+                ans++;
+                left--;
+                right++;
+            }
+        }
+        return ans;
+    }
+
+
+    /**
+     * 看不懂，先记录下
+     */
+    public int countSubstrings_1(String S) {
+        char[] A = new char[2 * S.length() + 3];
+        A[0] = '@';
+        A[1] = '#';
+        A[A.length - 1] = '$';
+        int t = 2;
+        for (char c : S.toCharArray()) {
+            A[t++] = c;
+            A[t++] = '#';
+        }
+
+        int[] Z = new int[A.length];
+        int center = 0, right = 0;
+        for (int i = 1; i < Z.length - 1; ++i) {
+            if (i < right)
+                Z[i] = Math.min(right - i, Z[2 * center - i]);
+            while (A[i + Z[i] + 1] == A[i - Z[i] - 1])
+                Z[i]++;
+            if (i + Z[i] > right) {
+                center = i;
+                right = i + Z[i];
+            }
+        }
+        int ans = 0;
+        for (int v : Z) ans += (v + 1) / 2;
+        return ans;
+    }
+}
