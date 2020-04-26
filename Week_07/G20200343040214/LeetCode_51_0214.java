dd871d3851820f3cbde448164bf8276f90ecbcd9
lijiakui <lijk@smartbow.net>
2020-04-26 19:22:42 +0800

    第七周作业

---
+++ Week_07/G20200343040214/LeetCode_51_0214.java
@@ -0,0 +1,47 @@
+class Solution51 {
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                board[i][j] = '.';
+        List<List<String>> res = new ArrayList<>();
+        dfs(board, 0, res);
+        return res;
+    }
+
+    private static void dfs(char[][] board, int colIndex, List<List<String>> res) {
+        if (colIndex == board.length) {
+            res.add(construct(board));
+            return;
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            if (validate(board, i, colIndex)) {
+                board[i][colIndex] = 'Q';
+                dfs(board, colIndex + 1, res);
+                board[i][colIndex] = '.';
+            }
+        }
+    }
+
+    private static boolean validate(char[][] board, int x, int y) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < y; j++) {
+                // na pie 列
+                if (board[i][j] == 'Q' && (x - y == i - j || x + y == i + j || x == i))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static List<String> construct(char[][] board) {
+        List<String> res = new LinkedList<>();
+        for (char[] chars : board) {
+            String s = new String(chars);
+            res.add(s);
+        }
+        return res;
+    }
+}
