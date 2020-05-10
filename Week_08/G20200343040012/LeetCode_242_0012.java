2a0ead82f6af63e0bcc5fa4c6b02004b339423cf
王戎 <88343264@qq.com>
2020-05-10 23:53:09 +0800

    update 1.第八周习题。

---
+++ Week_08/G20200343040012/LeetCode_242_0012.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    /**
+     * 暴力题解，直接两个排序后equals
+     * 时间复杂度：O(nlogn) 空间复杂度O(1)
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        return Arrays.equals(sChars, tChars);
+    }
+
+    public boolean isAnagram_1(String s, String t) {
+        int[] alphabet = new int[26];
+        for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
+        for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
+        for (int i : alphabet) if (i != 0) return false;
+        return true;
+    }
+
+}
