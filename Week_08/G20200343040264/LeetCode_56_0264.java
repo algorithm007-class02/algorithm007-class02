7e3e58acf61de87ea470d0b82a87f05df5c2af93
blackcat <7632566>
2020-05-11 11:17:04 +0800

    0264_Week08（java）

---
+++ Week_08/G20200343040264/LeetCode_56_0264.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (v1, v2) -> v1[0] - v2[0]);
+        int[][] res = new int[intervals.length][2];
+        int idx = -1;
+        for (int[] interval: intervals) {
+            if (idx == -1 || interval[0] > res[idx][1]) {
+                res[++idx] = interval;
+            } else {
+                res[idx][1] = Math.max(res[idx][1], interval[1]);
+            }
+        }
+        //去除多余的00数据
+        return Arrays.copyOf(res, idx + 1);
+    }
+}
