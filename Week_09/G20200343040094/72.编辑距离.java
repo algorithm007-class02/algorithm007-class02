a44738f6af4e4fa8e4775e6f1fc46347c83c2254
varian <mizeyuyu@gmail.com>
2020-05-12 17:29:33 +0800

    update

---
+++ Week_09/G20200343040094/72.编辑距离.java
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode.cn id=72 lang=java
+ *
+ * [72] 编辑距离
+ *
+ * https://leetcode-cn.com/problems/edit-distance/description/
+ *
+ * algorithms
+ * Hard (59.16%)
+ * Likes:    849
+ * Dislikes: 0
+ * Total Accepted:    57.1K
+ * Total Submissions: 96.6K
+ * Testcase Example:  '"horse"\n"ros"'
+ *
+ * 给你两个单词 word1 和 word2，请你计算出将 word1 转换成 word2 所使用的最少操作数 。
+ * 
+ * 你可以对一个单词进行如下三种操作：
+ * 
+ * 
+ * 插入一个字符
+ * 删除一个字符
+ * 替换一个字符
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：word1 = "horse", word2 = "ros"
+ * 输出：3
+ * 解释：
+ * horse -> rorse (将 'h' 替换为 'r')
+ * rorse -> rose (删除 'r')
+ * rose -> ros (删除 'e')
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：word1 = "intention", word2 = "execution"
+ * 输出：5
+ * 解释：
+ * intention -> inention (删除 't')
+ * inention -> enention (将 'i' 替换为 'e')
+ * enention -> exention (将 'n' 替换为 'x')
+ * exention -> exection (将 'n' 替换为 'c')
+ * exection -> execution (插入 'u')
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; ++i) dp[i][0] = i;
+        for (int j = 0; j <= n; ++j) dp[0][j] = j;
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) dp[i][j] = dp[i-1][j-1];
+                else {
+                    dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
+// @lc code=end
+
---
+++ Week_09/G20200343040094/746.使用最小花费爬楼梯.java
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=746 lang=java
+ *
+ * [746] 使用最小花费爬楼梯
+ *
+ * https://leetcode-cn.com/problems/min-cost-climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (48.27%)
+ * Likes:    283
+ * Dislikes: 0
+ * Total Accepted:    31.4K
+ * Total Submissions: 64.9K
+ * Testcase Example:  '[0,0,0,0]'
+ *
+ * 数组的每个索引做为一个阶梯，第 i个阶梯对应着一个非负数的体力花费值 cost[i](索引从0开始)。
+ * 
+ * 每当你爬上一个阶梯你都要花费对应的体力花费值，然后你可以选择继续爬一个阶梯或者爬两个阶梯。
+ * 
+ * 您需要找到达到楼层顶部的最低花费。在开始时，你可以选择从索引为 0 或 1 的元素作为初始阶梯。
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: cost = [10, 15, 20]
+ * 输出: 15
+ * 解释: 最低花费是从cost[1]开始，然后走两步即可到阶梯顶，一共花费15。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+ * 输出: 6
+ * 解释: 最低花费方式是从cost[0]开始，逐个经过那些1，跳过cost[3]，一共花费6。
+ * 
+ * 
+ * 注意：
+ * 
+ * 
+ * cost 的长度将会在 [2, 1000]。
+ * 每一个 cost[i] 将会是一个Integer类型，范围为 [0, 999]。
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        for (int i = 2; i < cost.length; ++i) {
+            cost[i] += Math.min(cost[i-1], cost[i-2]);
+        }
+        return Math.min(cost[cost.length - 1], cost[cost.length - 2]);
+    }
+}
+// @lc code=end
+
