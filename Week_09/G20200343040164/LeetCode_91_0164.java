5dfc6266ff735b42a2fca33c908856eacaa911ae
hjz520499 <619295640@qq.com>
2020-05-17 23:34:09 +0800

    week09

---
+++ Week_09/G20200343040164/LeetCode_91_0164.java
@@ -0,0 +1,73 @@
+//一条包含字母 A-Z 的消息通过以下方式进行了编码： 
+//
+// 'A' -> 1
+//'B' -> 2
+//...
+//'Z' -> 26
+// 
+//
+// 给定一个只包含数字的非空字符串，请计算解码方法的总数。 
+//
+// 示例 1: 
+//
+// 输入: "12"
+//输出: 2
+//解释: 它可以解码为 "AB"（1 2）或者 "L"（12）。
+// 
+//
+// 示例 2: 
+//
+// 输入: "226"
+//输出: 3
+//解释: 它可以解码为 "BZ" (2 26), "VF" (22 6), 或者 "BBF" (2 2 6) 。
+// 
+// Related Topics 字符串 动态规划
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0) return 0;
+        int len = s.length();
+        int help = 1;
+        int res = 0;
+        if (s.charAt(len - 1) != '0')
+            res = 1;
+
+        for (int i = len - 2; i >= 0; i--) {
+            if (s.charAt(i) == '0') {
+                help = res;
+                res = 0;
+                continue;
+            }
+            if ((s.charAt(i) - '0') * 10 + (s.charAt(i + 1) - '0') <= 26) {
+                res += help;
+                help = res - help;
+            } else help = res;
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+/**
+ * public int numDecodings(String s) {
+ *         if (s == null || s.length() == 0) return 0;
+ *         int len = s.length();
+ *         int[] dp = new int[len + 1];
+ *         dp[len] = 1;
+ *         if (s.charAt(len - 1) == '0')
+ *             dp[len - 1] = 0;
+ *         else dp[len - 1] = 1;
+ *
+ *         for (int i = len - 2; i >= 0; i--) {
+ *             if (s.charAt(i) == '0') {
+ *                 dp[i] = 0;
+ *                 continue;
+ *             }
+ *             if ((s.charAt(i) - '0') * 10 + (s.charAt(i + 1) - '0') <= 26)
+ *                 dp[i] = dp[i + 1] + dp[i + 2];
+ *             else dp[i] = dp[i + 1];
+ *         }
+ *         return dp[0];
+ *     }
+ */
